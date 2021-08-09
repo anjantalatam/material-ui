30ad19f92a2cecebcb2bb7e25dc5f2c67db695e7
Surya Prakash Anjan Talatam <tspa2117@gmail.com>
2021-08-09 12:19:11 +0530

    Revert code generated for Github pages

--- src/Material1.js
+++ src/Material1.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import DeleteIon from "@material-ui/icons/DeleteRounded";
-import { Delete as DeleteIcon } from "@material-ui/icons";
+// import DeleteIon from "@material-ui/icons/DeleteRounded";
+// import { Delete as DeleteIcon } from "@material-ui/icons";
 function Material1() {
   return <h2 align='center'>hello</h2>;
 }
